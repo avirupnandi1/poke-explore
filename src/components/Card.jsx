81c74452075589d062ee54b076a7f90a3c6a391c
Avirup Nandi <avirup.nandi651@gmail.com>
2024-01-26 23:25:50 +0530

    poke commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card=()=>{
+    return (
+        <>
+        <div className="card">
+        <h2>1</h2>
+        <img src='./images/pika.jpg' alt='/'/>
+        <h2>Pikachu</h2>
+        </div>
+      
+       
+    
+        </>
+    )
+}
+export default Card;

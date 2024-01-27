a72d29c8ccba0f0cc35f9d30c0cecaed02943788
Avirup Nandi <avirup.nandi651@gmail.com>
2024-01-27 17:13:57 +0530

    Poke fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-
-const Card=()=>{
+import React from "react";
+const Card = ({ pokemon, loading,infoPokemon}) => {
+   // console.log(pokemon);
     return (
-        <>     
-          <div className="card">
-            <div className="card-content"> 
-              <h1>1</h1>
-              <img src='./images/pika.jpg' alt='Pikachu' />
-              <h2>Pikachu</h2>
-            </div>
-          </div>
-    
-      </>
+        <>
+        {
+            loading ? <h1>Loading...</h1> :
+                pokemon.map((item) => {
+                    return (
+                        <>
+                            <div className="card">
+                                <h2>{item.id}</h2>
+                                <img src={item.sprites.front_default} alt="" />
+                                <h2>{item.name}</h2>
+                            </div>
+                        </>
+                    )
+                })
+        }
+
+        </>
     )
 }
 export default Card;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,67 @@
-import React  from 'react'
+import React, { useEffect, useState }  from 'react'
 import Card from './Card';
 import Header from './header';
+import axios from "axios";
+import Pokeinfo from './pokeinfo';
 const Main=()=>{
-    return(
-        <>
-          <Header/>
-     <div className="container">
- <div className="left-content">
-   <Card/>
- 
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <div className="btn-group"><button>Previous</button>
-    <button>Next</button></div>
- 
- </div>
-<div className="right-content"></div>
- {/* <Pokeinfo/> */}
-     </div>
-        
-        </>
-    )
+   const [pokeData, setPokeData]=useState([]);
+   const[loading,setLoading]=useState(true);
+   const [url,setUrl]=useState(" https://pokeapi.co/api/v2/pokemon/");
+   const [nextUrl,setNextUrl]=useState();
+   const [prevUrl,setprevUrl]=useState();
+   const [pokeDex,setPokeDex]=useState();
+
+   const pokeFun=async()=>{
+    setLoading(true);
+    const res=await axios.get(url);
+    setNextUrl(res.data.next);
+    setprevUrl(res.data.previous);
+    getPokemon(res.data.results);
+    setLoading(false);
+       
+   }
+ const getPokemon=async(res)=>{
+   res.map(async(item)=>{
+       const result= await axios.get(item.url)
+       setPokeData(state=>{
+         state=[...state,result.data]
+         state.sort((a,b)=>a.id>b.id?1:-1)
+         return state;
+       })
+   })
+ }
+
+
+
+ useEffect(()=>{
+     pokeFun();
+ },[url])
+
+ return(
+   <>
+   <Header/>
+       <div className="container">
+           <div className="left-content">
+               <Card pokemon={pokeData} loading={loading} infoPokemon={poke=>setPokeDex(poke)}/>
+               
+               <div className="btn-group">
+                   {  prevUrl && <button onClick={()=>{
+                       setPokeData([])
+                      setUrl(prevUrl) 
+                   }}>Previous</button>}
+
+                   { nextUrl && <button onClick={()=>{
+                       setPokeData([])
+                       setUrl(nextUrl)
+                   }}>Next</button>}
+
+               </div>
+           </div>
+           <div className="right-content">
+              {/* <Pokeinfo data={pokeDex}/> */}
+           </div>
+       </div>
+   </>
+)
 }
 export default Main;
